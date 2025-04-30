371c5d42be0deee73d0bb70a4ea2dfbfba9d1df2
Arseny <20096ninja@gmail.com>
2025-04-30 15:46:37 +0300

    [ ADD ] plot curve from file

--- main.py
+++ main.py
@@ -52,6 +52,19 @@ def generate_random_dots(start, end, num_mid_points, aspect=1.0, alpha_jitter=0.
     return result
 
 
+def plot_cpp_solution():
+    global plt
+    with open(f"../../CProjects/TraceBezierCurve/out", "r") as file:
+        x, y = [], []
+        for line in file.readlines():
+            a, b = map(float, line.split())
+            if a == 0 and b == 0 and x:
+                break
+            x.append(a)
+            y.append(b)
+        plt.plot(x, y, 'm')
+
+
 border = []
 points = generate_random_dots([0, 0],
                               [5, 5.5],
@@ -59,12 +72,22 @@ points = generate_random_dots([0, 0],
                               aspect=0.2,
                               alpha_jitter=0.1)
 
+# points = np.array([
+#     [0, 0],
+#     [1, 4],
+#     [2, 1],
+#     [3, 3],
+#     [4, 2],
+#     [3, 1],
+# ])
+
 plt.figure(figsize=(8, 8))
 curve = get_curve(points, num_points=50)
-plt.xlim(-1, 6)
-plt.ylim(-1, 6)
+# plt.xlim(-1, 6)
+# plt.ylim(-1, 6)
 plt.plot(*zip(*points), 'ro', label='Control Points')
 plt.plot(*zip(*curve), 'b-', label='Bezier Curve')
 plt.plot(*zip(*border), 'g--', label='Border')
+plot_cpp_solution()
 plt.show()
 
